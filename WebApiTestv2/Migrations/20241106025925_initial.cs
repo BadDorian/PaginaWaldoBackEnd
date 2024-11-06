c7f9271a519c98fe0df93bae7269fdd41a2bf9cd
Matias Pacheco <mapacheco75@gmail.com>
2024-11-06 00:09:47 -0300

    migrations

---
+++ WebApiTestv2/Migrations/20241106025925_initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiTestv2.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CarritoItems_ProductoId",
+                table: "CarritoItems",
+                column: "ProductoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarritoItems_Productos_ProductoId",
+                table: "CarritoItems",
+                column: "ProductoId",
+                principalTable: "Productos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarritoItems_Productos_ProductoId",
+                table: "CarritoItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarritoItems_ProductoId",
+                table: "CarritoItems");
+        }
+    }
+}
